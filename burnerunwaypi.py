a0781d47d4d4f841cf9da60866fdaaced5cff58c
Gal Bracha <galbra@gmail.com>
2017-05-04 20:11:45 +0300

    Typos

--- burnerunwaypi.py
+++ burnerunwaypi.py
@@ -1,12 +1,13 @@
 import time
 import serial
+from serial import SerialException 
 
 ARDUINO_PORT = '/dev/ttyUSB0'
 BAUD_RATE = 9600
 
 def main():
   isConnected = False
-  while !isConnected:
+  while not isConnected:
     isConnected = tryToConnect()
 
   while 1:
@@ -17,18 +18,18 @@ def tryToConnect():
     ser = serial.Serial(ARDUINO_PORT, BAUD_RATE)
     print "Connected!"
     return True
-  except SerialException se:
+  except SerialException as se:
     print "Cant connect. sleeping for 2 seconds and retrying"
     time.sleep(2)
     return False
 
 def tryToRead():
   try:
-      line = ser.readline()
-      print line
-    except SerialException se:
-      print "Oops. probably arduino got disconnected"
-      print se
+    line = ser.readline()
+    print line
+  except SerialException as se:
+    print "Oops. probably arduino got disconnected"
+    print se
 
 if __name__ == "__main__":
   main()
