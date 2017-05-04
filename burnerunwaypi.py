3c74b55c43e20a15b6db019a6c98688f94944636
Gal Bracha <galbra@gmail.com>
2017-05-04 20:09:07 +0300

    Added exception handling waiting for arduino to connect and sleeping in case of exception

--- burnerunwaypi.py
+++ burnerunwaypi.py
@@ -1,10 +1,34 @@
+import time
 import serial
-ser = serial.Serial('/dev/ttyUSB0', 9600)
+
+ARDUINO_PORT = '/dev/ttyUSB0'
+BAUD_RATE = 9600
 
 def main():
+  isConnected = False
+  while !isConnected:
+    isConnected = tryToConnect()
+
   while 1:
-    line = ser.readline()
-    print line
+    tryToRead()
+
+def tryToConnect():
+  try:
+    ser = serial.Serial(ARDUINO_PORT, BAUD_RATE)
+    print "Connected!"
+    return True
+  except SerialException se:
+    print "Cant connect. sleeping for 2 seconds and retrying"
+    time.sleep(2)
+    return False
+
+def tryToRead():
+  try:
+      line = ser.readline()
+      print line
+    except SerialException se:
+      print "Oops. probably arduino got disconnected"
+      print se
 
 if __name__ == "__main__":
   main()
